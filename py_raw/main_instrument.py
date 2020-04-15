086900849207ca27665138f91f97ad579b928c99
sushchikh <a.sushchikh@gmail.com>
2020-04-15 16:31:14 +0300

    add email sender

--- py_raw/main_instrument.py
+++ py_raw/main_instrument.py
@@ -3,6 +3,8 @@ from modules import get_data_from_file
 from modules import get_df_with_prices_instr
 from modules import push_data_to_xlsx
 from modules import get_df_with_prices_makita
+from modules import push_data_to_csv
+from modules import email_sender
 
 if __name__ == '__main__':
     instr_df, makita_df = get_data_from_file()
@@ -14,8 +16,12 @@ if __name__ == '__main__':
     sleep(0.2)
     code_prices_makita_df = get_df_with_prices_makita(makita_df)
     name = 'instrument_prices'
-    push_data_to_xlsx(name, code_prices_instr_df)
+    # push_data_to_xlsx(name, code_prices_instr_df)
+    print('push Instrument to csv')
+    push_data_to_csv(name, code_prices_instr_df)
     name = 'makita_prices'
-    push_data_to_xlsx(name, code_prices_makita_df)
+    # push_data_to_xlsx(name, code_prices_makita_df)
+    print('push Makita to csv')
+    push_data_to_csv(name, code_prices_makita_df)
     # print(code_prices_df.head())
-
+    email_sender()
--- py_raw/modules.py
+++ py_raw/modules.py
@@ -1,10 +1,9 @@
 import pandas as pd
 import requests
+import smtplib
 from bs4 import BeautifulSoup as bs
 from tqdm import tqdm
-from datetime import datetime
-
-
+from email.message import EmailMessage
 
 
 ######## ########    ###    ######## ##     ## ########  ########  ######
@@ -24,6 +23,37 @@ def price_cutter(text: str):
         if i.isdigit() or i == '.' or i == ',':
             output_message += i
     return str(output_message)
+
+
+# ------------------------------------------------------------------------------------------------
+# send files to email
+def email_sender():
+    email_pass = 'tvcabitgcvuqalje'
+    email_user = 'a.sushchikh@gmail.com'
+    # contacts_list = ['info@td-stroybat.ru']
+    contacts_list = ['a89638857493@gmail.com']
+
+    msg = EmailMessage()
+    msg['Subject'] = 'Instrument, Makita prices'
+    msg['From'] = 'email_user'
+    msg['To'] = ', '.join(contacts_list)
+    msg.set_content(
+        ' ')
+    files_xlsx = []
+
+    files_xlsx.append('./../xlsx/instrument_prices.csv')
+    files_xlsx.append('./../xlsx/makita_prices.csv')
+
+    for file in files_xlsx:
+        with open(file, 'rb') as f:
+            file_data = f.read()
+            file_name = f.name
+
+        msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
+
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+        smtp.login(email_user, email_pass)
+        smtp.send_message(msg)
 
 
 ########     ###    ########    ###
@@ -49,7 +79,6 @@ def get_data_from_file():
     return instr_df_with_our_code, makita_df_with_our_code
 
 
-# TODO func must take "name" arg and make file with that arg in path
 # ------------------------------------------------------------------------------------------------
 # push data to xlsx-file, decorate it
 def push_data_to_xlsx(name, data):
@@ -73,6 +102,12 @@ def push_data_to_xlsx(name, data):
     # worksheet.freeze_panes(1, 0)
     writer.save()
 
+
+# ------------------------------------------------------------------------------------------------
+# push dataframe to csv
+def push_data_to_csv(name, data):
+    file_path = './../xlsx/' + str(name) + '.csv'
+    data.to_csv(file_path, sep=';', header=False)
 
 ########     ###    ########   ######  #### ##    ##  ######
 ##     ##   ## ##   ##     ## ##    ##  ##  ###   ## ##    ##
