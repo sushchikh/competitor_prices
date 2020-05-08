067ebf867aab4a0494627d0e48ebc555691a7d77
sushchikh <a.sushchikh@gmail.com>
2020-05-08 13:44:22 +0300

    instead 3 parsing all items, forget it in last commit

--- py_raw/vse_instrumenti_parser.py
+++ py_raw/vse_instrumenti_parser.py
@@ -39,7 +39,7 @@ def get_data_from_file():
 # parsing
 def get_prices_code_dict(browser, df):
     code_price_dict = {}
-    for i in tqdm(range(3)):  # len(df['ссылка'])
+    for i in tqdm(range(len(df['ссылка']))):  # len(df['ссылка'])
         try:
             browser.get(df['ссылка'][i])
             item_price = browser.find_element_by_css_selector('#b-product-info > div.right-block.card-right-aside > div.card-basket-block-new > div.ns.price-wrapper > div > span.price-value')
